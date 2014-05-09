dc6ac382039a598b73ccf32a9289a5b2d46a7084
Nick Hatter <nickhatter@gmail.com>
2014-05-10 00:09:13 +0100

    Remove vibration for now + make UI look nicer

--- Assets/Scripts/Flashy.cs
+++ Assets/Scripts/Flashy.cs
@@ -38,8 +38,8 @@ public class Flashy : MonoBehaviour {
 	GUIContent counterDisplay = new GUIContent();
 	GUIContent umbrellaDisplay = new GUIContent();
 
-	Rect CENTER_SCREEN = new Rect(Screen.width/4 - 25, Screen.height/2, Screen.width/2 + 75, 300);
-	Rect TOP_RIGHT_SCREEN = new Rect(Screen.width - 75, 0, 100, 100);
+	Rect CENTER_SCREEN = new Rect(Screen.width/4 - 75, Screen.height/2 - 100, Screen.width/2 + 150, 400);
+	Rect TOP_RIGHT_SCREEN = new Rect(Screen.width - 100, 0, 100, 125);
 
 	// Use this for initialization
 	void Start () {
@@ -55,88 +55,76 @@ public class Flashy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		#if UNITY_IOS || UNITY_ANDROID
-		if(isVibrating) {
-			vibrateTimer += Time.deltaTime;
-			Handheld.Vibrate();
+		if(!gameOver) {
+			// Now, assuming it isn't game over...
+			if(!isFadingIn && !isFlashCaught && !gameOver) {
+				if(flashTimer > timeToFlash) {
+					iTween.CameraFadeTo(iTween.Hash("amount", 1.0f, "time", flashInTime, "oncompletetarget", this.gameObject, "oncomplete", "fadeOutFlash"));
+					isFadingIn = true;
+					savedByUmbrella = false;
+				} else {
+					if(isTouchReleased && (Input.touchCount > 0 || Input.GetMouseButton(0))) {
+						if(umbrellaCount > 0 && !savedByUmbrella) {
+							savedByUmbrella = true;
+							umbrellaCount--;
+							umbrellaDisplay.text = ""+umbrellaCount;
+						} else {
+							flashTimer = 0;
+							dodgeCount = 0;
+							gameOver = true;
+							counterDisplay.text = ""+dodgeCount;
 
-			if(vibrateTimer >= timeToVibrate) {
-				vibrateTimer = 0;
-				isVibrating = false;
+							isVibrating = true;
+						}
+
+						isTouchReleased = false;
+					}
+				}
 			}
-		}
-		#endif
 
-		if(!isFadingIn && !isFlashCaught && !gameOver) {
-			if(flashTimer > timeToFlash) {
-				iTween.CameraFadeTo(iTween.Hash("amount", 1.0f, "time", flashInTime, "oncompletetarget", this.gameObject, "oncomplete", "fadeOutFlash"));
-				isFadingIn = true;
-				savedByUmbrella = false;
-			} else {
-				if(isTouchReleased && (Input.touchCount > 0 || Input.GetMouseButton(0))) {
-					if(umbrellaCount > 0 && !savedByUmbrella) {
+			if(isFadingIn) {
+				if(isTouchReleased && !isFlashCaught && flashTimer >= timeToFlash && flashTimer <= timeToFlash+flashInTime+reactionLeeway && (Input.touchCount > 0 || Input.GetMouseButton(0))) {
+					dodgeCount++;
+
+					if(dodgeCount % 5 == 0) {
+						congratulatoryPhrase = congratulatoryPhrases[Mathf.RoundToInt(Random.value*(congratulatoryPhrases.Length-1))];
+					}
+
+					counterDisplay.text = ""+dodgeCount;
+					isFlashCaught = true;
+					isTouchReleased = false;
+				}
+
+
+				if(flashTimer > timeToFlash+flashInTime+reactionLeeway && !isFlashCaught & !gameOver) {
+					if(umbrellaCount > 0) {
 						savedByUmbrella = true;
 						umbrellaCount--;
 						umbrellaDisplay.text = ""+umbrellaCount;
+						isFlashCaught = true;
 					} else {
-						flashTimer = 0;
 						dodgeCount = 0;
 						gameOver = true;
 						counterDisplay.text = ""+dodgeCount;
-						audio.PlayOneShot(zapSound);
-
-							isVibrating = true;
-
+						Debug.Log("Missed the flash");
 					}
-
-					isTouchReleased = false;
-				}
-			}
-		}
-
-		if(isFadingIn) {
-			if(isTouchReleased && !isFlashCaught && flashTimer >= timeToFlash && flashTimer <= timeToFlash+flashInTime+reactionLeeway && (Input.touchCount > 0 || Input.GetMouseButton(0))) {
-				dodgeCount++;
-
-				if(dodgeCount % 5 == 0) {
-					congratulatoryPhrase = congratulatoryPhrases[Mathf.RoundToInt(Random.value*(congratulatoryPhrases.Length-1))];
 				}
 
-				counterDisplay.text = ""+dodgeCount;
-				isFlashCaught = true;
-				isTouchReleased = false;
 			}
 
-
-			if(flashTimer > timeToFlash+flashInTime+reactionLeeway && !isFlashCaught & !gameOver) {
-				if(umbrellaCount > 0) {
-					savedByUmbrella = true;
-					umbrellaCount--;
-					umbrellaDisplay.text = ""+umbrellaCount;
-					isFlashCaught = true;
-				} else {
-					dodgeCount = 0;
-					gameOver = true;
-					counterDisplay.text = ""+dodgeCount;
-					Debug.Log("Missed the flash");
-					audio.PlayOneShot(zapSound);
-				}
+			#if UNITY_EDITOR
+			if(Input.GetMouseButtonUp(0)) {
+				isTouchReleased = true;
+				Debug.Log("Touch released");
 			}
+			#else
+			if(Input.touchCount == 0) {
+				isTouchReleased = true;
+			}
+			#endif
 
+			flashTimer += Time.deltaTime;
 		}
-
-		#if UNITY_EDITOR
-		if(Input.GetMouseButtonUp(0)) {
-			isTouchReleased = true;
-			Debug.Log("Touch released");
-		}
-		#else
-		if(Input.touchCount == 0) {
-			isTouchReleased = true;
-		}
-		#endif
-
-		flashTimer += Time.deltaTime;
 	}
 
 
@@ -152,7 +140,6 @@ public class Flashy : MonoBehaviour {
 		isFlashCaught = false;
 		isFadingIn = false;
 		flashTimer = 0;
-		savedByUmbrella = false;
 		calculateTimeToFlash();
 	}
 
@@ -170,6 +157,7 @@ public class Flashy : MonoBehaviour {
 					if(GUILayout.Button("RETRY")) {
 						gameOver = false;
 						initFlash();
+						savedByUmbrella = false;
 					}
 
 					if(GUILayout.Button("BUY UMBRELLAS")) {
@@ -180,6 +168,10 @@ public class Flashy : MonoBehaviour {
 			if(savedByUmbrella) {
 				GUI.Label (CENTER_SCREEN, "PHEW, THAT WAS CLOSE!");
 			} else {
+				if(dodgeCount == 1) {
+					GUI.Label(CENTER_SCREEN, "YOU'VE GOT IT!");
+				}
+
 				if(dodgeCount%5 == 0 && dodgeCount != 0) {
 					GUI.Label(CENTER_SCREEN, congratulatoryPhrase);
 				}
