f1684e0ca6c652e69150520d24cb0f88f8e05ad5
Nick Hatter <nickhatter@gmail.com>
2014-07-21 02:00:10 +0100

    Remove uncompilable code

--- Assets/giftgaming/Scripts/giftgamingGifts.cs
+++ Assets/giftgaming/Scripts/giftgamingGifts.cs
@@ -7,11 +7,24 @@ public class giftgamingGifts : MonoBehaviour {
 	public static void openNextGift() {
 		giftgaming.use.nextGift();
 		giftgaming.use.openGift();
-		// Implement your own custom actions here for gift codes
-		Flashy.use.unlockIAP(giftgaming.use.currentGift.giftCode);
-		giftgaming.use.setInGameGiftLogo(Flashy.use.umbrellaIcon);
+
+		/** Implement your own custom actions here for gift codes
+		 * 
+		 * Example:
+		 * 
+		 * Flashy.use.unlockIAP(giftgaming.use.currentGift.giftCode);
+		 * 
+		 * // Set icon to use when showing the user what gift they got
+		 * // Note: you probably want a different logo for each gift
+		 * giftgaming.use.setInGameGiftLogo(Flashy.use.umbrellaIcon);
+		 * 
+		 */
 	}
 
+	/* Description functions:
+	 * In case you wanted to explain what the gift does for the player
+	 * 
+	 */
 	public static string getGiftDescription() {
 		return getGiftDescription(giftgaming.use.currentGift.giftCode);
 	}
